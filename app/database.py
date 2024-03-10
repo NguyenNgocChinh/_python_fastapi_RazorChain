f8c70d35e743ff3937e01e9d2f9e3d3dc7a22a13
Chinh <chinh@Chinhs-MacBook-Pro.local>
2024-03-10 22:04:53 +0700

    init sourc

---
+++ app/database.py
@@ -0,0 +1,54 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv 
+import os
+ 
+load_dotenv()
+
+    
+DB_HOST = os.getenv("DB_HOST") 
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+ 
+# With locally
+# SQLALCHEMY_DATABASE_URL = "mysql://{USER}:{PASS}@{HOST}:{PORT}/{DB_NAME}".format(
+#     HOST=DB_HOST,
+#     DB_NAME=MYSQL_DATABASE,
+#     PASS=MYSQL_PASSWORD,
+#     USER=MYSQL_USER,
+#     PORT=MYSQL_PORT
+# )
+
+# With docker
+SQLALCHEMY_DATABASE_URL = "mysql://{USER}:{PASS}@{HOST}/{DB_NAME}".format(
+    HOST='db',
+    DB_NAME=MYSQL_DATABASE, 
+    PASS=MYSQL_PASSWORD,
+    USER=MYSQL_USER,
+)
+
+print(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+
+meta= MetaData()
+
+conection=engine.connect()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
